e216d07731ca8009669fb6fda38d49983a2007f9
esdepee <esdepeee@gmail.com>
2019-05-09 19:15:50 +0200

    -all

---
+++ routes/furniture.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const furnitureCtrl = require('../controllers/furniture');
+
+router.get('/', furnitureCtrl.getAllFurniture);
+router.get('/:id', furnitureCtrl.getOneFurniture);
+router.post('/order', furnitureCtrl.orderFurniture);
+
+console.log(router);
+
+module.exports = router;
